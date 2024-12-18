678feba7f085aa64d73201ad7ba9a5abf128320c
william-nicolussi <188550990+william-nicolussi@users.noreply.github.com>
2024-12-18 19:04:27 +0100

    Q1 ok

--- Q1/puttftp.c
+++ Q1/puttftp.c
@@ -1,24 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-void print_usage(const char *prog_name) {
-    printf("Usage:\n");
-    printf("%s <host> <file>\n", prog_name);
-    printf("  <host>: TFTP server address\n");
-    printf("  <file>: File to upload\n");
+#define DISPLAY_SHELL_ERROR_MESSAGE "\nError: Unable to write.\n"
+#define CHAR_BUFFER_SIZE 256
+
+#define MESSAGE_WRONG_LAUNCH "You launched the code without the proper arguments.\n"
+#define USAGE_FORMAT "%s <host> <file>\n"
+#define USAGE_HOST_INFO "\t<host>: TFTP server address\n"
+#define USAGE_FILE_INFO "\t<file>: File to upload\n"
+
+#define MESSAGE_UPLOAD_FORMAT "Uploading file '%s' from server '%s'...\n"
+
+// Display the message or print perror and exit
+void displayString(char* strToWrite)
+{
+	if(write(STDOUT_FILENO, strToWrite, strlen(strToWrite))==-1)
+	{
+		perror(DISPLAY_SHELL_ERROR_MESSAGE);
+        exit(EXIT_FAILURE);
+	}
+}
+
+void printHowToUse(char *progName)
+{   
+	displayString(MESSAGE_WRONG_LAUNCH);
+    char buffer[CHAR_BUFFER_SIZE];
+    snprintf(buffer, sizeof(buffer), USAGE_FORMAT, progName);
+    displayString(buffer);
+    displayString(USAGE_HOST_INFO);
+    displayString(USAGE_FILE_INFO);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        print_usage(argv[0]);
+    if (argc != 3)
+    {
+        printHowToUse(argv[0]);
         return EXIT_FAILURE;
     }
 
-    const char *host = argv[1];
-    const char *file = argv[2];
+    char *hostName = argv[1];
+    char *fileName = argv[2];
 
-    printf("Uploading file '%s' to server '%s'...\n", file, host);
-    // Placeholder for TFTP protocol implementation
+    //Use buffer to hold the final formatted message, then print the informations
+    char buffer[CHAR_BUFFER_SIZE];
+    snprintf(buffer, sizeof(buffer), MESSAGE_UPLOAD_FORMAT, fileName, hostName);
+    displayString(buffer);
+    
     return EXIT_SUCCESS;
 }
