ba2ac013b801ec01fee9d889af9e2ea1a2d10034
william-nicolussi <188550990+william-nicolussi@users.noreply.github.com>
2024-12-19 11:08:08 +0100

    Q3 ok

--- Q3/puttftp.c
+++ Q3/puttftp.c
@@ -60,6 +60,43 @@ void printResolvedIPAddress(char *hostName, struct addrinfo *res)
     }
 }
 
+void createSocket(struct addrinfo *res, int *sockfd, char *hostName)
+{
+    static char strFormattedInfo[CHAR_BUFFER_SIZE];
+    *sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (*sockfd < 0)
+    {
+        displayMessage(MESSAGE_SOCKET_ERROR);
+        exit(EXIT_FAILURE);
+    }
+
+    snprintf(strFormattedInfo, sizeof(strFormattedInfo), MESSAGE_SOCKET_RESERVED, hostName);
+    displayMessage(strFormattedInfo);
+    
+	close(*sockfd);
+}
+
+void resolveIPAddress(char *hostName, struct addrinfo **res)
+{
+    static char strFormattedInfo[CHAR_BUFFER_SIZE];
+    struct addrinfo hints;
+    
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;      // IPv4
+    hints.ai_socktype = SOCK_DGRAM; // UDP (TFTP)
+    
+    int status = getaddrinfo(hostName, TFTP_PORT, &hints, res);
+    if (status != 0)
+    {
+        snprintf(strFormattedInfo, sizeof(strFormattedInfo), MESSAGE_GETADDR_ERROR, hostName);
+        displayMessage(strFormattedInfo);
+        exit(EXIT_FAILURE);
+    }
+    
+    int sockfd;
+    createSocket(*res, &sockfd, hostName);
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 3)
@@ -74,31 +111,14 @@ int main(int argc, char *argv[])
     snprintf(strFormattedInfo, sizeof(strFormattedInfo), MESSAGE_RESOLVING_ADDRESS, hostName);
     displayMessage(strFormattedInfo);
 
-    struct addrinfo hints, *res;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET; // IPv4
-    hints.ai_socktype = SOCK_DGRAM; // UDP (TFTP)
-    int status = getaddrinfo(hostName, TFTP_PORT, &hints, &res);
-    if (status != 0)
-    {
-        snprintf(strFormattedInfo, sizeof(strFormattedInfo), MESSAGE_GETADDR_ERROR, hostName);
-        displayMessage(strFormattedInfo);
-        return EXIT_FAILURE;
-    }
+    struct addrinfo *res;
+    resolveIPAddress(hostName, &res);
     printResolvedIPAddress(hostName, res);
-
-    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sockfd < 0)
-    {
-        displayMessage(MESSAGE_SOCKET_ERROR);
-        freeaddrinfo(res);
-        return EXIT_FAILURE;
-    }
+    
     snprintf(strFormattedInfo, sizeof(strFormattedInfo), MESSAGE_SOCKET_RESERVED, hostName);
     displayMessage(strFormattedInfo);
     snprintf(strFormattedInfo, sizeof(strFormattedInfo), MESSAGE_READY_UPLOAD, fileName, hostName);
     displayMessage(strFormattedInfo);
-    close(sockfd);
 
     return EXIT_SUCCESS;
 }
