44fd2c7d39dd341d8959cb17c379bd290207fc45
william-nicolussi <188550990+william-nicolussi@users.noreply.github.com>
2024-12-20 20:45:08 +0100

    Q4 ok

--- Q4/gettftp.c
+++ Q4/gettftp.c
@@ -31,6 +31,19 @@
 #define MESSAGE_SOCKET_ERROR "Error: Could not create socket.\n"
 #define TFTP_PORT "1069" /* TFTP default port as a string for getaddrinfo */
 
+#define MESSAGE_ERROR_OPENING "Failed to open file for writing\n"
+#define MESSAGE_ERROR_WRITE_FILE "Failed to write data to file\n"
+#define MODE_OPEN_FUNCTION 0644 /*owner: R&W(6); group: R(4); others: read(4)*/
+#define TRANSFER_MODE "octet" /*binary transfer mode*/
+#define MESSAGE_RQQ_FAILED "Failed to send RRQ\n"
+#define MESSAGE_RQQ_SENT "RRQ sent for file '%s'\n"
+#define MESSAGE_ACK_FAILED "Failed to send ACK\n"
+#define MESSAGE_ACK_SENT "ACK sent for block %d\n"
+#define MESSAGE_DATA_RECEIVED_FAILED "Failed to receive data\n"
+#define MESSAGE_RECEIVED_SERVER "Error received from server: %s\n"
+#define MESSAGE_RECEIVED_BLOCK "Received block %d\n"
+#define MESSAGE_TRANSFER_COMPLETED "File transfer complete.\n"
+
 void displayMessage(char* strToWrite)
 {
 	if(write(STDOUT_FILENO, strToWrite, strlen(strToWrite))==-1)
@@ -50,55 +63,46 @@ void printHowToUse(char *progName)
     displayMessage(USAGE_FILE_INFO);
 }
 
-void handleError(const char *message)
+void saveToFile(char *fileName, char *data, size_t dataSize)
 {
-    displayMessage((char *)message);
-    exit(EXIT_FAILURE);
-}
-
-void saveToFile(const char *fileName, const char *data, size_t dataSize)
-{
-    int fd = open(fileName, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    int fd = open(fileName, O_WRONLY | O_CREAT | O_APPEND, MODE_OPEN_FUNCTION);
     if (fd == -1)
     {
-        handleError("Failed to open file for writing\n");
+        displayMessage(MESSAGE_ERROR_OPENING);
+        exit(EXIT_FAILURE);
     }
-
     if (write(fd, data, dataSize) == -1)
     {
         close(fd);
-        handleError("Failed to write data to file\n");
+        displayMessage(MESSAGE_ERROR_WRITE_FILE);
+        exit(EXIT_FAILURE);
     }
 
     close(fd);
 }
 
-void sendRRQ(int sockfd, struct sockaddr_in *serverAddr, socklen_t addrLen, const char *filename)
+void sendRRQ(int sockfd, struct sockaddr_in *serverAddr, socklen_t addrLen, char *filename)
 {
-    char buffer[CHAR_BUFFER_SIZE];
+    char buffer[CHAR_BUFFER_SIZE]; //buffer of bytes
     int offset = 0;
 
-    // Opcode for RRQ (2 bytes)
     buffer[offset++] = 0;
-    buffer[offset++] = RRQ_OPCODE;
-
-    // Filename
+    buffer[offset++] = RRQ_OPCODE; //add read request
     strcpy(&buffer[offset], filename);
     offset += strlen(filename) + 1;
-
-    // Transfer mode ("octet")
-    strcpy(&buffer[offset], "octet");
-    offset += strlen("octet") + 1;
+    strcpy(&buffer[offset], TRANSFER_MODE);
+    offset += strlen(TRANSFER_MODE) + 1; //+1 because of '\0'
 
     // Send RRQ packet
     if (sendto(sockfd, buffer, offset, 0, (struct sockaddr *)serverAddr, addrLen) == -1)
     {
-        handleError("Failed to send RRQ\n");
+        displayMessage(MESSAGE_RQQ_FAILED);
+        exit(EXIT_FAILURE);
     }
 
-    displayMessage("RRQ sent for file '\n");
-    displayMessage((char *)filename);
-    displayMessage("'\n");
+    char message[CHAR_BUFFER_SIZE];
+    snprintf(message, sizeof(message), MESSAGE_RQQ_SENT, filename);
+    displayMessage(message);
 }
 
 void sendACK(int sockfd, struct sockaddr_in *serverAddr, socklen_t addrLen, uint16_t blockNumber)
@@ -108,23 +112,22 @@ void sendACK(int sockfd, struct sockaddr_in *serverAddr, socklen_t addrLen, uint
     // Opcode for ACK (2 bytes)
     buffer[0] = 0;
     buffer[1] = ACK_OPCODE;
-
-    // Block number (2 bytes)
-    buffer[2] = (blockNumber >> 8) & 0xFF;
-    buffer[3] = blockNumber & 0xFF;
+    buffer[2] = (blockNumber >> 8); // Extract higher byte
+    buffer[3] = blockNumber; // Extract lower byte
 
     // Send ACK packet
     if (sendto(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)serverAddr, addrLen) == -1)
     {
-        handleError("Failed to send ACK\n");
+        displayMessage(MESSAGE_ACK_FAILED);
+        exit(EXIT_FAILURE);
     }
 
     char message[CHAR_BUFFER_SIZE];
-    snprintf(message, sizeof(message), "ACK sent for block %d\n", blockNumber);
+    snprintf(message, sizeof(message), MESSAGE_ACK_SENT, blockNumber);
     displayMessage(message);
 }
 
-void receiveFile(int sockfd, struct sockaddr_in *serverAddr, socklen_t addrLen, const char *fileName)
+void getFileFromServer(int sockfd, struct sockaddr_in *serverAddr, socklen_t addrLen, char *fileName)
 {
     char buffer[CHAR_BUFFER_SIZE];
     int receivedBytes;
@@ -135,21 +138,32 @@ void receiveFile(int sockfd, struct sockaddr_in *serverAddr, socklen_t addrLen, 
         receivedBytes = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)serverAddr, &addrLen);
         if (receivedBytes == -1)
         {
-            handleError("Failed to receive data\n");
+            displayMessage(MESSAGE_DATA_RECEIVED_FAILED);
+            exit(EXIT_FAILURE);
+        }
+
+        uint16_t opcode = (buffer[0] << 8) | buffer[1];
+
+        // Check if it's an error packet
+        if (opcode == 5)
+        {
+            char errorMessage[CHAR_BUFFER_SIZE];
+            snprintf(errorMessage, sizeof(errorMessage), MESSAGE_RECEIVED_SERVER, &buffer[4]);
+            displayMessage(errorMessage);
+            exit(EXIT_FAILURE);
         }
 
         // Check if it's a DATA packet
-        if (buffer[1] == DATA_OPCODE)
+        if (opcode == DATA_OPCODE)
         {
             uint16_t blockNumber = (buffer[2] << 8) | buffer[3];
-
             if (blockNumber == expectedBlock)
             {
                 // Save received data to file
                 saveToFile(fileName, &buffer[4], receivedBytes - 4);
 
                 char message[CHAR_BUFFER_SIZE];
-                snprintf(message, sizeof(message), "Received block %d\n", blockNumber);
+                snprintf(message, sizeof(message), MESSAGE_RECEIVED_BLOCK, blockNumber);
                 displayMessage(message);
 
                 // Send ACK
@@ -160,7 +174,7 @@ void receiveFile(int sockfd, struct sockaddr_in *serverAddr, socklen_t addrLen, 
                 // Check if it's the last packet (less than 512 bytes of data)
                 if (receivedBytes < CHAR_BUFFER_SIZE)
                 {
-                    displayMessage("File transfer complete.\n");
+                    displayMessage(MESSAGE_TRANSFER_COMPLETED);
                     break;
                 }
             }
@@ -202,13 +216,14 @@ void resolveIPAddress(char *hostName, struct addrinfo **res)
     }
 }
 
-void receiveFileSocket(char *fileName, struct addrinfo *res)
+void receiveFile(char *fileName, struct addrinfo *res)
 {
   // Create socket
     int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (sockfd == -1)
     {
-        handleError("Failed to create socket\n");
+        displayMessage(MESSAGE_SOCKET_ERROR);
+        exit(EXIT_FAILURE);
     }
 
     struct sockaddr_in serverAddr;
@@ -219,7 +234,7 @@ void receiveFileSocket(char *fileName, struct addrinfo *res)
     sendRRQ(sockfd, &serverAddr, sizeof(serverAddr), fileName);
 
     // Receive file
-    receiveFile(sockfd, &serverAddr, sizeof(serverAddr), fileName);
+    getFileFromServer(sockfd, &serverAddr, sizeof(serverAddr), fileName);
 
     close(sockfd);
 }
@@ -234,12 +249,10 @@ int main(int argc, char *argv[])
     char *hostName = argv[1];
     char *fileName = argv[2];
 
-    // Resolve server address
     struct addrinfo *res;
     resolveIPAddress(hostName, &res);
     printResolvedIPAddress(hostName, res);
-
-    receiveFileSocket(fileName, res);
+    receiveFile(fileName, res);
    
     return EXIT_SUCCESS;
 }
